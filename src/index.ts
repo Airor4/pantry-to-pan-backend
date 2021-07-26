c1e53f9b0003671ab921adb5f38bcab5d8fd5e41
Airor <airor@bmal.net>
2021-07-26 15:41:09 -0400

    added cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-import express from 'express'
+import express from 'express';
+// @ts-ignore
+import cors from 'cors';
+import 'dotenv/config';
+
 import userRouter from './routes/user';
 import queryRouter from './routes/query';
+
 
 const app = express();
 
 const port = 3000;
+
+app.use(cors({"origin": process.env.FRONTEND_ORIGIN}));
+// app.options('*', cors())
 
 app.get('/', (req, res) => {
   res.send('welcome to the recipe api');
@@ -13,6 +21,6 @@ app.get('/', (req, res) => {
 app.use('/user', userRouter);
 app.use('/query', queryRouter);
 
-app.listen(port, () => {
+app.listen(process.env.port || port, () => {
   console.log(`recipe backend listening at port: ${port}`);
 });
