166b3f2cedafae3066d80974030ee276a281f211
Airor <airor@bmal.net>
2021-08-07 12:25:03 -0400

    cors tweak

--- src/index.ts
+++ src/index.ts
@@ -20,14 +20,6 @@ const port = process.env.PORT || 8080;
 
 // We only need to listen to the port if we are in a dev environment because the load balancer
 // automatically forwards requests to our app for us on Elastic Beanstalk
-if (process.env.DEV_ENV ) {
-  app.use(cors({origin: 'http://localhost:4200'}));
-  app.listen(port, () => {
-    console.log(`app listening on ${port}`);
-  });
-} else {
-  app.use(cors({origin: 'https://pantrytopan.org'}));
-}
 
 app.get('/', (req, res) => {
   res.send('welcome to the recipe api');
@@ -35,5 +27,14 @@ app.get('/', (req, res) => {
 
 app.use('/user', userRouter);
 app.use('/query', queryRouter);
+
+if (process.env.DEV_ENV ) {
+  app.use(cors({origin: 'http://localhost:4200'}));
+  app.listen(port, () => {
+    console.log(`app listening on ${port}`);
+  });
+} else {
+  app.use(cors({origin: 'https://pantrytopan.org*'}));
+}
 
 
