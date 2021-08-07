5c155a99605716547b6934dd34eaf13609512e24
Airor <airor@bmal.net>
2021-08-07 12:00:10 -0400

    cors rework

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,24 @@ import queryRouter from './routes/query';
 const app = express();
 
 // 8080 for nginx and eb
-//const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
-app.use(cors({origin: 'https://pantrytopan.org'}));
-// app.options('*', cors())
+// We only need to listen to the port if we are in a dev environment because the load balancer
+// automatically forwards requests to our app for us on Elastic Beanstalk
+if (process.env.DEV_ENV ) {
+  app.use(cors({origin: 'http://localhost:4200'}));
+  app.listen(port, () => {
+    console.log(`app listening on ${port}`);
+  });
+} else {
+  app.use(cors({origin: 'https://pantrytopan.org'}));
+}
 
 app.get('/', (req, res) => {
   res.send('welcome to the recipe api');
