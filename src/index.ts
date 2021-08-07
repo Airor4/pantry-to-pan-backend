dbe3fe8355303a6851378473a5694cd777740c26
Airor <airor@bmal.net>
2021-08-07 12:55:27 -0400

    logging for cors production settings

--- src/index.ts
+++ src/index.ts
@@ -12,15 +12,6 @@ const app = express();
 // 8080 for nginx and eb
 const port = process.env.PORT || 8080;
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-// We only need to listen to the port if we are in a dev environment because the load balancer
-// automatically forwards requests to our app for us on Elastic Beanstalk
-
 app.get('/', (req, res) => {
   res.send('welcome to the recipe api');
 });
@@ -34,7 +25,8 @@ if (process.env.DEV_ENV ) {
     console.log(`app listening on ${port}`);
   });
 } else {
-  app.use(cors({origin: 'https://pantrytopan.org*'}));
+  console.log('Cors enabled for prod');
+  app.use(cors({origin: 'https://pantrytopan.org'}));
 }
 
 
