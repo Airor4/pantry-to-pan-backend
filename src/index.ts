adcf39d180e85c8f3c5d1ccf0296ef526e3e2eee
Airor <airor@bmal.net>
2021-08-07 14:28:40 -0400

    cors tweak

--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,7 @@ if (process.env.DEV_ENV ) {
   app.use(cors({origin: 'http://localhost:4200'}));
 } else {
   console.log('Cors enabled for prod');
-  // app.use(cors({origin: 'https://pantrytopan.org'}));
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next()
-  });
+  app.use(cors({origin: 'https://pantrytopan.org'}));
 }
 
 app.get('/', (req, res) => {
