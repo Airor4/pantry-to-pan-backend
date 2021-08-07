1b25f6838a522121ce1a03acf050d15bd4b0e973
Airor <airor@bmal.net>
2021-08-07 11:30:35 -0400

    access-control cors middleware

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,12 @@ const app = express();
 // 8080 for nginx and eb
 //const port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors({origin: 'https://pantrytopan.org'}));
 // app.options('*', cors())
 
